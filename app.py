85147b5b49b0861d93a4953a4cce10d68e2893f1
Neet Shah <neetshah02@gmail.com>
2018-07-28 23:12:15 -0400

    created generic file with GET and POST requests using flask

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+            'name': 'My Item',
+            'price': 15.99
+            }
+        ]
+    }
+]
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+@app.route('store/<string:name>')
+def get_store(name):
+    pass
+
+@app.route('store/')
+def get_stores():
+    pass
+
+@app.route('store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+@app.route('store/<string:name>/item')
+def get_item_in_store(name):
+    pass
+
+app.run(port=5000)
