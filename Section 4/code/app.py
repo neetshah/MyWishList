72a009632f35be9283d5edfab3a321d3c5a5ca61
Neet Shah <neetshah02@gmail.com>
2018-07-29 21:33:17 -0400

    created simple flask RESTful Api

--- Section 4/code/app.py
+++ Section 4/code/app.py
@@ -3,3 +3,11 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+
+class Student(Resource):
+    def get(self, name):
+        return {'student': name}
+
+api.add_resource(Student, '/student/<string:name>') #http://127.0.0.1:5000/student/Dave
+
+app.run(port=5000)
