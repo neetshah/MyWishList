11ba7882aa0959fd9fb63ddf1c0a81da09d91846
Neet Shah <neetshah02@gmail.com>
2018-08-07 19:38:51 -0400

    created User object

--- Section 4/code/security.py
+++ Section 4/code/security.py
@@ -15,10 +15,19 @@ username_mapping = {
     }
 }
 
-user_id = {
+userid_mapping = {
     1: {
         'id': 1,
         'username': 'bob',
         'password': 'asdf'
     }
 }
+
+def authenticate(username, password):
+    user = username_mapping.get(username, None)
+    if user and user.password == password:
+        return user
+
+def identity(payload):
+    user_id = payload.get(identity)
+    return userid_mapping.get(user_id, None)
