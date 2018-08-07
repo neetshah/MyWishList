57d0e357a9163eff4f8edac17fca1ecb0369d8b2
Neet Shah <neetshah02@gmail.com>
2018-08-07 19:31:39 -0400

    added simple JWT authentication template

--- Section 4/code/app.py
+++ Section 4/code/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.secret_key = 'password'
 api = Api(app)
 
 items = []
@@ -14,6 +15,7 @@ class Item(Resource):
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
---
+++ Section 4/code/security.py
@@ -0,0 +1,24 @@
+users = [
+    {
+        'id': 1,
+        'username': 'bob',
+        'password': 'asdf'
+    }
+]
+
+
+username_mapping = {
+    'bob': {
+        'id': 1,
+        'username': 'bob',
+        'password': 'asdf'
+    }
+}
+
+user_id = {
+    1: {
+        'id': 1,
+        'username': 'bob',
+        'password': 'asdf'
+    }
+}
