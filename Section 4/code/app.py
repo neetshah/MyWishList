9e21b074f0c037ed94bf3a32f52b15454c0d4c00
Neet Shah <neetshah02@gmail.com>
2018-08-08 19:05:47 -0400

    Code how to delete an item

--- Section 4/code/app.py
+++ Section 4/code/app.py
@@ -1,16 +1,22 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'password'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)  # /auth
+
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'item': item), 200 if item is not None else 404
+        return {'item': item}, 200 if item is not None else 404
 
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
@@ -20,6 +26,11 @@ class Item(Resource):
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return({'message': 'Item Deleted'})
 
 class ItemList(Resource):
     def get(self):
--- Section 4/code/security.py
+++ Section 4/code/security.py
@@ -1,7 +1,8 @@
-From user import User
+from werkzeug.security import safe_str_cmp
+from user import User
 
 users = [
-    Users(1, 'bob', 'asdf')
+    User(1, 'bob', 'asdf')
 ]
 username_mapping = {u.username: u for u in users}
 
@@ -9,7 +10,7 @@ userid_mapping = {u.id: u for u in users}
 
 def authenticate(username, password):
     user = username_mapping.get(username, None)
-    if user and user.password == password:
+    if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload):
