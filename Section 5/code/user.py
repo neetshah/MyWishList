5740e561f35044552007c8090759556bfabb7c90
Neet Shah <shahn17@mcmaster.ca>
2018-08-25 00:56:38 -0400

    checks the username with the usernames in the SQL db for a match

--- Section 5/code/user.py
+++ Section 5/code/user.py
@@ -1,5 +1,22 @@
+import sqlite3
+
 class User(object):
     def __init__(self, id, username, password):
         self.id = id
         self.username = username
         self.password = password
+
+    def find_by_username(self, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connetion.cursor()
+
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username, ))
+        row = result.fetchone()
+        if row: 
+            user = User(row[0], row[1], row[2])
+        else:
+            user= None
+
+        connection.close()
+        return user
